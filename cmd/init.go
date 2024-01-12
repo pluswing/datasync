00bd38bc6a64d9dd882c8fb9b57c94327b2edd8b
pluswing <toshiyuki@pluswing.co.jp>
2024-01-12 10:24:48 +0900

    #031 initコマンド作成開始。

--- cmd/init.go
+++ cmd/init.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
@@ -11,13 +16,163 @@ var initCmd = &cobra.Command{
 	Short: "generate datasync.yaml",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
+			fmt.Printf("could not start program: %s\n", err)
+			os.Exit(1)
+		}
 		// Bubble teaを使って、UIを作る。
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+}
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle.Copy()
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle.Copy()
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Copy().Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
+const SCREEN_SELECT_TARGET_KIND = 1
+const SCREEN_INPUT_MYSQL = 2
+const SCREEN_INPUT_FILES = 3
+const SCREEN_CONFIRM_ADD_TARGET = 4
+const SCREEN_CONFIRM_SETUP_REMOTE = 5
+const SCREEN_SELECT_REMOTE_KIND = 6
+const SCREEN_INPUT_GCS = 7
+
+type model struct {
+	screenType int
+
+	// 入力共有
+	focusIndex int
+	inputs     []textinput.Model
+
+	// TODO
+}
+
+func initialModel() model {
+	m := model{
+		screenType: SCREEN_SELECT_TARGET_KIND,
+		inputs:     make([]textinput.Model, 3),
+	}
+
+	// var t textinput.Model
+	// for i := range m.inputs {
+	// 	t = textinput.New()
+	// 	t.Cursor.Style = cursorStyle
+	// 	t.CharLimit = 32
+
+	// 	switch i {
+	// 	case 0:
+	// 		t.Placeholder = "Nickname"
+	// 		t.Focus()
+	// 		t.PromptStyle = focusedStyle
+	// 		t.TextStyle = focusedStyle
+	// 	case 1:
+	// 		t.Placeholder = "Email"
+	// 		t.CharLimit = 64
+	// 	case 2:
+	// 		t.Placeholder = "Password"
+	// 		t.EchoMode = textinput.EchoPassword
+	// 		t.EchoCharacter = '•'
+	// 	}
+
+	// 	m.inputs[i] = t
+	// }
+
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch m.screenType {
+	case SCREEN_SELECT_TARGET_KIND:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "ctrl+c", "esc":
+				return m, tea.Quit
+			case "up":
+				m.focusIndex -= 1
+			case "down":
+				m.focusIndex += 1
+			case "enter":
+				// TODO
+				m.screenType = SCREEN_INPUT_MYSQL
+			}
+		}
+	case SCREEN_INPUT_MYSQL:
+	default:
+		panic("invalid screenType")
+		// cmds := make([]tea.Cmd, len(m.inputs))
+		// for i := 0; i <= len(m.inputs)-1; i++ {
+		// 	if i == m.focusIndex {
+		// 		// Set focused state
+		// 		cmds[i] = m.inputs[i].Focus()
+		// 		m.inputs[i].PromptStyle = focusedStyle
+		// 		m.inputs[i].TextStyle = focusedStyle
+		// 		continue
+		// 	}
+		// 	// Remove focused state
+		// 	m.inputs[i].Blur()
+		// 	m.inputs[i].PromptStyle = noStyle
+		// 	m.inputs[i].TextStyle = noStyle
+		// }
+
+		// return m, tea.Batch(cmds...)
+	}
+
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	// for i := range m.inputs {
+	// 	b.WriteString(m.inputs[i].View())
+	// 	if i < len(m.inputs)-1 {
+	// 		b.WriteRune('\n')
+	// 	}
+	// }
+	switch m.screenType {
+	case SCREEN_SELECT_TARGET_KIND:
+		b.WriteString("? How kind of dump target? …\n")
+		if m.focusIndex == 0 {
+			b.WriteString("❯ MySQL\n")
+			b.WriteString("  File(s)\n")
+		} else {
+			b.WriteString("  MySQL\n")
+			b.WriteString("❯ File(s)\n")
+		}
+	}
+
+	return b.String()
 }
 
 /*
--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	cloud.google.com/go/storage v1.30.1
 	github.com/aliakseiz/go-mysqldump v1.0.2
 	github.com/briandowns/spinner v1.23.0
+	github.com/charmbracelet/bubbles v0.17.1
+	github.com/charmbracelet/bubbletea v0.25.0
+	github.com/charmbracelet/lipgloss v0.9.1
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/google/uuid v1.3.1
 	github.com/mcuadros/go-defaults v1.2.0
@@ -22,7 +25,10 @@ require (
 	cloud.google.com/go/compute v1.23.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v1.1.1 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fatih/color v1.15.0 // indirect
@@ -35,15 +41,23 @@ require (
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pingcap/errors v0.11.5-0.20210425183316-da1aaba5fb63 // indirect
 	github.com/pingcap/failpoint v0.0.0-20220801062533-2eaa32854a6c // indirect
 	github.com/pingcap/log v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
